2aff0abea895cd7bc3a776e170dcf215ba48acce
Maron <web-spinner@mail.ru>
2020-09-30 20:53:53 +1000

    edit fonts-include preload-include from tasks

--- gulp/tasks/preload-include.js
+++ gulp/tasks/preload-include.js
@@ -5,12 +5,15 @@ const paths  = require('../paths');
 const fs     = require('fs');
 
 const preload = done => {
-  const file = paths.write.preload;
-  fs.writeFile(file, '', ()=>{});
-  config.preload.map(item => {
-    let link = `<link rel="preload" href="${item.href}" as="${item.as}">\r\n`;
-    fs.appendFile(file, link , ()=>{});
-  })
+  const filePath = paths.write.preload;
+  const file = fs.readFileSync(filePath)
+  if(file == ''){
+    fs.writeFile(filePath, '', ()=>{});
+    config.preload.map(item => {
+      let link = `<link rel="preload" href="${item.href}" as="${item.as}">\r\n`;
+      fs.appendFile(filePath, link , ()=>{});
+    })
+  }
   done();
 }
 
