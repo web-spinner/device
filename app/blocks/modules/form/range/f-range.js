d18c98d3b665be039f4e25c577fcbbdc605927c0
Maron <web-spinner@mail.ru>
2021-01-12 19:28:35 +1000

    edit scripts f-range

--- app/blocks/modules/form/range/f-range.js
+++ app/blocks/modules/form/range/f-range.js
@@ -1,55 +1,59 @@
-const slider = document.getElementById('filter-range');
+(function(){
+  'use strict'
+  window.addEventListener('DOMContentLoaded', () => {
 
-const createSlider = slider => {
-  if(slider) {
-    const sliderMax = parseInt( slider.getAttribute('data-max') )
-    const tooltipFirst = parseInt( slider.getAttribute('data-tooltip-first') )
-    const tooltipLast = parseInt( slider.getAttribute('data-tooltip-last') )
-    noUiSlider.create(slider, {
-      start: [tooltipFirst, tooltipLast],
-      range: {
-        'min': 0,
-        'max': sliderMax 
-      },
-      step: 1,
-      tooltips: true,
-      connect: true,
-      format: {
-        to: function(value) {
-          return parseInt(value) 
-        },
-        from: function(value) {
-          return parseInt(value)
-        },
+    const slider = document.getElementById('filter-range');
+
+    const createSlider = slider => {
+      if(slider) {
+        const sliderMax = parseInt( slider.getAttribute('data-max') )
+        const tooltipFirst = parseInt( slider.getAttribute('data-tooltip-first') )
+        const tooltipLast = parseInt( slider.getAttribute('data-tooltip-last') )
+        noUiSlider.create(slider, {
+          start: [tooltipFirst, tooltipLast],
+          range: {
+            'min': 0,
+            'max': sliderMax 
+          },
+          step: 1,
+          tooltips: true,
+          connect: true,
+          format: {
+            to: function(value) {
+              return parseInt(value) 
+            },
+            from: function(value) {
+              return parseInt(value)
+            },
+          }
+        })
       }
-    })
-  }
-}
+    }
 
-const formatSlider = (slider, values, handle) => {
-  if(slider){
-    const tooltips = document.querySelectorAll('.noUi-tooltip')
-    tooltips[handle].innerHTML = (handle == 0) ? 'от ' + values[handle] : 'до ' + values[handle]
-  }
-}
+    const formatSlider = (slider, values, handle) => {
+      if(slider){
+        const tooltips = document.querySelectorAll('.noUi-tooltip')
+        tooltips[handle].innerHTML = (handle == 0) ? 'от ' + values[handle] : 'до ' + values[handle]
+      }
+    }
 
-const setValues = slider => {
-  if(slider){
-    const input = document.querySelector('.f-range__input')
-    input.value = slider.noUiSlider.get()
-  }
-}
+    const setValues = slider => {
+      if(slider){
+        const input = document.querySelector('.f-range__input')
+        input.value = slider.noUiSlider.get()
+      }
+    }
 
-const eventSlider = (event, slider) => {
-  if(slider){
-    slider.noUiSlider.on(event, function (values, handle) {
-      formatSlider(slider, values, handle)
-      setValues(slider)
-    })
-  }
-}
+    const eventSlider = (event, slider) => {
+      if(slider){
+        slider.noUiSlider.on(event, function (values, handle) {
+          formatSlider(slider, values, handle)
+          setValues(slider)
+        })
+      }
+    }
 
-createSlider(slider)
-eventSlider('update', slider)
-
-
+    createSlider(slider)
+    eventSlider('update', slider)
+  })
+}());
