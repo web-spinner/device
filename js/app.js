6f866c2f75e3b4e22a4ddd37650e81462e855019
Maron <web-spinner@mail.ru>
2021-01-15 18:54:37 +1000

    Updates

--- js/app.js
+++ js/app.js
@@ -146,29 +146,30 @@
   })
 }());
 
-ymaps.ready(init);
-function init(){
-  var myMap = new ymaps.Map("map", {
-    center: [55.68697956906804, 37.52965449999998],
-    zoom: 17 
-  })
+if(document.querySelector('#map')){
+  ymaps.ready(init);
+  function init(){
+    var myMap = new ymaps.Map("map", {
+      center: [55.68697956906804, 37.52965449999998],
+      zoom: 17 
+    })
 
-  var myPin = new ymaps.GeoObjectCollection({}, {
-    draggable: false,
-    iconLayout: 'default#image',
-    iconImageHref: 'img/map-pin.png',
-    iconImageSize: [22, 43],
-    iconImageOffset: [-10, -55]
-  });
+    var myPin = new ymaps.GeoObjectCollection({}, {
+      draggable: false,
+      iconLayout: 'default#image',
+      iconImageHref: 'img/map-pin.png',
+      iconImageSize: [22, 43],
+      iconImageOffset: [-10, -55]
+    });
 
-  var myPlacemark = new ymaps.Placemark([55.68697956906804, 37.52965449999998], {
-    hintContent: '<span class="map-popup__hint">улица Строителей, 15</span>',
-  });
+    var myPlacemark = new ymaps.Placemark([55.68697956906804, 37.52965449999998], {
+      hintContent: '<span class="map-popup__hint">улица Строителей, 15</span>',
+    });
 
-  myPin.add(myPlacemark);
-  myMap.geoObjects.add(myPin);
+    myPin.add(myPlacemark);
+    myMap.geoObjects.add(myPin);
+  }
 }
-
 
 
 
